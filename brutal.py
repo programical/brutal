a4e8e286603a6ba6d7ae26c1253b2099bcd39764
programical <32033530+programical@users.noreply.github.com>
2019-10-11 22:09:47 +0200

    add brutal.py

---
+++ brutal.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# github.com/programical
+# simple brute force algorithm
+
+# force(str/array, int, function)
+# returns False when out of options, returns True when callback does
+def force(symbols, length, callback):
+    return recursiveFor("", 1, symbols, length, callback)
+
+# use the force() wrapper instead of this
+def recursiveFor(prefix, currentLength, symbols, length, callback):
+    for symbol in symbols:
+        if currentLength < length:
+            currentLength += 1
+            
+            # if solved, exit recursion
+            if recursiveFor(prefix + symbol, currentLength, symbols, length, callback):
+                return True
+            
+            currentLength -= 1
+        else:
+            # callback function can return a truthy value to stop the bruteforcing
+            if callback(prefix + symbol):
+                return True
+
+    # out of combinations
+    return False
+
