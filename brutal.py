d39102a64d75f67c68e1dffe97d6413900cb4ccb
programical <32033530+programical@users.noreply.github.com>
2020-02-13 16:51:27 +0100

    simplify, merge recursion with wrapper

--- brutal.py
+++ brutal.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-# github.com/programical
-# simple brute force algorithm
 
 # returns False when out of options, returns True when callback does
-def force(symbols: list, length: int, callback: callable) -> bool:
-    return recursiveFor("", 1, symbols, length, callback)
-
-# use the force() wrapper instead of this
-def recursiveFor(prefix, currentLength, symbols, length, callback):
+def force(symbols: list, length: int, callback: callable, prefix = "", curLen = 1) -> bool:
     for symbol in symbols:
-        if currentLength < length:
-            currentLength += 1
+        if curLen < length:
+            curLen += 1
             
             # if solved, exit recursion
-            if recursiveFor(prefix + symbol, currentLength, symbols, length, callback):
+            if force(symbols, length, callback, prefix = prefix + symbol, curLen = curLen):
                 return True
             
-            currentLength -= 1
+            curLen -= 1
         else:
             # callback function can return a truthy value to stop the bruteforcing
             if callback(prefix + symbol):
